d56589ff738608138416e5a789d6c807eca218b1
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-03 22:33:55 +0530

    Create largest_of_3_no.java

---
+++ largest_of_3_no.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class largest_of_3_no {
+  public static void main(String args[]){
+    int a=7; b=3; c=6;
+    
+    if (a>=b && a>=c){
+      System.out.println("a is largest.");
+    }
+    else is (b>=c){
+      System.out.println("b is largest.");
+    }
+    else {
+      System.out.println("c is largest.");
+    }
+  }
+}
