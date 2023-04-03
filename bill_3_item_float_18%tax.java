966c23ecf6949693c39db8041b2ab10d59374fa0
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-04 01:00:40 +0530

    Create bill_3_item_float_18%tax.java

---
+++ bill_3_item_float_18%tax.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class bill_3_item_float_18%tax {
+  public static void main (String args []){
+    System.out.println("Provide the prices of all items : ");
+    Scanner sc = new Scanner (System.in);
+    float pen= sc.nextFloat();
+    float pencil = sc.nextFloat();
+    float eraser = sc.nextFloat();
+    float total = pen + pencil + eraser;
+    System.out.println("Total bill without tax : " + total);
+     //now for tax 18%
+    
+    float nettotal = total + (total * 0.18f);
+    System.out.print("Total bill with 18% tax : " + nettotal);
+  }
+}
