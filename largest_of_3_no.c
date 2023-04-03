afa1017799c95d36db0ade6062a5c2b2472b456c
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-03 22:27:27 +0530

    Update largest_of_3_no.c

--- largest_of_3_no.c
+++ largest_of_3_no.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+void main (){
+  int a=7, b=3, c=6;
+  if (a>=b && a>=c){
+    printf("a is largest.");
+  }
+  else if (b>=c){
+    printf("b is largest.");
+  }
+  else {
+    printf("c is largest.");
+  }
+}
