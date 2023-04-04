2cf7729cdf89a1006c0ecce6c9cab4f7cfc64752
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-04 22:27:48 +0530

    Create pass_or_fail.java

---
+++ pass_or_fail.java
@@ -0,0 +1,7 @@
+public class pass_or_fail {
+    public static void main(String args[]) {
+        int marks = 67;
+        String rc = (marks >= 33) ? "Pass" : "Fail";
+        System.out.println(rc);
+    }
+}
