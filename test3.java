58aa3b411392ab53c8f3e32c497bb8a8244a3435
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-04 21:42:59 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -3,7 +3,7 @@ public class test3{
     int x=2, y=5;
     int exp1 = (x*y/x);
     int exp2 = (x*(y/x));
-    System.out.println(exp1 + ",");
-    System.out.println(exp2);
+    System.out.print(exp1 + ",");
+    System.out.print(exp2);
   }
 }
