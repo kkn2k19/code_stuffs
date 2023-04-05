8d5826387cd83668e745ee6672a33d7e2ba9edea
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-05 22:32:45 +0530

    Create positive_negative.java

---
+++ positive_negative.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class positive_negative{
+  public static void main (String args []){
+    Scanner sc=new Scanner (System.in);
+    int n = sc.nextInt ();
+    (n>0)? System.out.println("positive number.") : System.out.println("negative number.");
+  }
+}
