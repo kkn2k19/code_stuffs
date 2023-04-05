b3eb5b32d5fafcf36f3937905a7febcf7dea37e1
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-05 22:39:22 +0530

    Update positive_negative.java

--- positive_negative.java
+++ positive_negative.java
@@ -1,16 +1,14 @@
 import java.util.*;
 
-public class positive_negative{
-  public static void main (String args []){
-    Scanner sc=new Scanner (System.in);
-    int n = sc.nextInt ();
-    if (n>0)
-    {
-      System.out.println("positive number.");
+public class positive_negative {
+    public static void main(String args[]) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Provide a number : ");
+        int n = Sc.nextInt();
+        if (n > 0) {
+            System.out.println("positive number.");
+        } else {
+            System.out.println("negative number.");
+        }
     }
-    else 
-    {
-      System.out.println("negative number.");
-    }
-  }
 }
