7d662153be64e177137af2ffbf8be702d7ceaee6
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-05 19:13:05 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class calculator{
+  public static void main (String args[]){
+    Scanner sc=new Scanner (System.in);
+    System.out.println("Provide num1 : ");
+    int a = sc.nextInt();
+    System.out.println("Provide num2 : ");
+    int b = sc.nextInt();
+    System.out.println("select the operator : ");
+    char operator = sc.next().Char(0);
