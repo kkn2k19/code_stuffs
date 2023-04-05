d894d4ed04e9d61dad257dcd9dc6e28e0bfcc21f
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-05 20:07:07 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main (){
+    int num1, num2, operator;
+    printf("Provide num1 : ");
+    scanf("%d", &num1);
+    printf("Provide num2 : ");
+    scanf("%d", &num2);
+    printf("select the operator : ");
+    scanf("%c", &operator) // character is scanned like this only as it doesnot have next.Char()
+                                             // syntax.
+
+        switch (operator) {
+            case '+':
+                printf(a + b);
+                break;
+            case '-':
+                printf(a - b);
+                break;
+            case '*':
+                printf(a * b);
+                break;
+            case '/':
+                printf(a / b);
+                break;
+            case '%':
+                printf(a % b);
+                break;
+            default:
+                printf("Wrong Operator.");
+        }
+    }
+}
