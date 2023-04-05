077d7471ebbcde7c253f6b8a27b9d72891fbb2b4
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-06 00:04:12 +0530

    Create test6.java

---
+++ test6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class test6 {
+    public static void main(String args[]) {
+        int a = 63, b = 36;
+        boolean x = (a < b) ? true : false; // x=false
+        int y = (a > b) ? a : b; // y=63
+        System.out.println(y);
+    }
+
+}
