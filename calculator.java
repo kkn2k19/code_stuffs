1857b8e0a45e87f23a23d4508b8e0f3591c8f3f4
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-05 20:00:04 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,29 +1,34 @@
 import java.util.*;
 
-public class calculator{
-  public static void main (String args[]){
-    Scanner sc=new Scanner (System.in);
-    System.out.println("Provide num1 : ");
-    int a = sc.nextInt();
-    System.out.println("Provide num2 : ");
-    int b = sc.nextInt();
-    System.out.println("select the operator : ");
-    char operator = sc.next().CharAt(0);     //character is scanned like this only as it doesnot have next.Char() syntax.
-    
-    switch (operator){
-        Case '+' : System.out.println(a+b);
-        break;
-        Case '-': System.out.println(a-b);
-        break;
-        Case '*' : System.out.println(a*b);
-        break;
-        Case '/' : System.out.println(a/b);
-        break;
-        Case '%' : System.out.println(a%b);
-        break;
-      default: System.out.println ("Wrong Operator.");
-    }
-  }
-}
+public class calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide num1 : ");
+        int a = sc.nextInt();
+        System.out.println("Provide num2 : ");
+        int b = sc.nextInt();
+        System.out.println("select the operator : ");
+        char operator = sc.next().charAt(0); // character is scanned like this only as it doesnot have next.Char()
+                                             // syntax.
 
-    
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("Wrong Operator.");
+        }
+    }
+}
