d750552ba7903a217245a6e93847f528f2121f18
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-07 01:52:58 +0530

    Create 1_to_10.java

---
+++ 1_to_10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class kkn_hundred_times_for {
+    public static void main(String args[]) {
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            System.out.println("KKN");
+        }
+    }
+}
