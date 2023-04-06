a3872c9478541d03e174cac165a13af385fc9b09
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-07 02:37:24 +0530

    Create break_at_multiple_of_10.java

---
+++ break_at_multiple_of_10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class break_at_multiple_of_10 {
+  public static void main (String args []){
+    Scanner sc=new Scanner (System.in);
+    
+    for ( ; ; ){
+      int n=sc.nextInt();
+      if (n%i==0){
+        break;
+      }
+    }
+  }
+}
