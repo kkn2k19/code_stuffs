8bf4127c5b7e5ec3abde5dfb2e9c946afb4e4e5f
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-07 01:54:25 +0530

    Update 1_to_10.java

--- 1_to_10.java
+++ 1_to_10.java
@@ -1,10 +1,27 @@
 import java.util.*;
-
-public class kkn_hundred_times_for {
-    public static void main(String args[]) {
-        int n = 100;
-        for (int i = 1; i <= n; i++) {
-            System.out.println("KKN");
-        }
+public class 1_to_10{
+  public static void main (String args[]){
+    System.out.println("Provide n : ");
+    //Scanner sc= new Scanner (System.in);
+    
+    //int n = sc.nextInt();
+      int n=10;
+    for (int i = 1; i<=n; i++){
+      System.out.println(i);
     }
+    
+    /* 
+    int i=1;
+    while(i<=n){
+    System.out.println(i);
+    }
+    
+    
+    int i=1;
+    do {
+    System.out.println(i);
+    } while (i<=n);
+    */
+    
+  }
 }
