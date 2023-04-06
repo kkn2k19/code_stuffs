22a1c796b4df1fda86548b8107a06c77e7231791
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-04-07 01:57:05 +0530

    Create basicPattern1.java

---
+++ basicPattern1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class basicPattern1 {
+  public static void main (String args[]){
+    //System.out.println("Provide n : ");
+    //Scanner sc= new Scanner (System.in);
+    
+    //int n = sc.nextInt();
+    int n=4;
+   
+    for (int i = 1; i<=n; i++){
+      System.out.println("****");
+    }
+    
+    /* 
+    int i=1;
+    while(i<=n){
+    System.out.println("****");
+    }
+    
+    
+    int i=1;
+    do {
+    System.out.println("****");
+    } while (i<=n);
+    */
+    
+  }
+}
